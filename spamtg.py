2bfa55e8ece1b3fdc7e1cc8f1c794ab346dde27f
SeRgEy2701 <54076281+SeRgEy2701@users.noreply.github.com>
2020-02-25 20:33:58 +0300

    Add files via upload

---
+++ spamtg.py
@@ -0,0 +1,18 @@
+from telethon.sync import TelegramClient, events
+
+#SERGOPROXY TG:@sergey2701,darksploit:SergoProxy
+print("'TG - spam' - скрипт написан SergoProxy, для спама в личных сообщения телеграм. \n Все вопросы и идеи в TG: @sergey0804")
+print()
+hashtg = input("Введите хэш аккаунта: ")
+iptg = int(input("Введите ip приложения: "))
+px = int(input("Введите количество раз: "))
+idp = input("Введите id пользователя: ")
+mes = input("Текст сообщения: ")
+
+api_id = iptg
+api_hash = hashtg
+
+for i in range(px):
+	with TelegramClient('proxy', api_id, api_hash) as client:
+		client.send_message(idp, mes)
+#SERGOPROXY TG:@sergey2701,darksploit:SergoProxy
