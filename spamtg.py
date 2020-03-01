570488d456cd9090f85bf411078669b19c13f3e1
SeRgEy2701 <54076281+SeRgEy2701@users.noreply.github.com>
2020-03-01 12:38:31 +0300

    Update spamtg.py

--- spamtg.py
+++ spamtg.py
@@ -1,6 +1,6 @@
 from telethon.sync import TelegramClient, events
 
-#SERGOPROXY TG:@sergey2701,darksploit:SergoProxy
+#SERGOPROXY TG:@sergey0804,darksploit:SergoProxy
 print("'TG - spam' - скрипт написан SergoProxy, для спама в личных сообщения телеграм. \n Все вопросы и идеи в TG: @sergey0804")
 print()
 hashtg = input("Введите хэш аккаунта: ")
@@ -15,4 +15,4 @@ api_hash = hashtg
 for i in range(px):
 	with TelegramClient('proxy', api_id, api_hash) as client:
 		client.send_message(idp, mes)
-#SERGOPROXY TG:@sergey2701,darksploit:SergoProxy
+#SERGOPROXY TG:@sergey0804,darksploit:SergoProxy
